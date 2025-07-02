4626105cf90109002d1a98dffedc3406351b8af9
ZainMalik-984 <zee.zainmalik.dev@gmail.com>
2025-07-03 01:04:16 +0500

    vacations and classesapps are added

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls'))
+    path('api/user/', include('user.urls')),
+    path('api/vacations/', include('vacations.urls')),
+    path('api/classes/', include('classes.urls')),
 ]
